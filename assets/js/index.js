ae81ba6e66404b4e61bf0e9c4c03039cd93fada1
DopplerKuo <doppler.kuo@gmail.com>
2017-03-12 06:56:03 +0800

    INIT

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+$(window).load(function() {
+	$('.recommend-slide').unslider({
+		speed: 300,
+		nav: false
+	})
+
+	$('.instagram-btn').click(function(){
+		$('#loading').addClass('show')
+		$('#get_started').addClass('out')
+	})
+
+	$('.check-btn').click(function() {
+		$('#recommend').addClass('check')
+		$('.recommend-text').fadeOut(500)
+		$('.recommend-text').css('animation-name', 'slideLeft')
+	})
+	$('.check-back-btn').click(function() {
+		$('#recommend').removeClass('check')
+	})
+})
